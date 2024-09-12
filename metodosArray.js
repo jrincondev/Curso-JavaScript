094a668ae342664e7ae9e3441d9055cf7383d4af
jrincondev <jrincondev@gmail.com>
2024-09-12 17:40:07 -0500

    Update

---
+++ metodosArray.js
@@ -0,0 +1,17 @@
+// Transformadores
+// pop( ) => elimina el último elemento de un array y lo devuelve.
+
+let nombres = ["pedro", "brayan", "jorge"];
+
+console.log(nombres);
+
+resultado = nombres.pop();
+
+console.log(resultado);
+console.log(nombres);
+// shift( ) => elimina el primer elemento de un array y lo devuelve.
+// push( ) => agrega un elemento a1 array a1 final de la lista.
+// reverse( ) => invierte el orden de los elementos de un array.
+// unshift( ) => agrega uno o más elementos a1 inicio del array, y devuelve la nueva longitud del arraya
+// sort() => ordena los elementos de un arreglo (array) localmente y devuelve el arreglo ordenado.
+// splice( ) => cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
