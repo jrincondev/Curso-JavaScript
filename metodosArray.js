e903d659198a39516f5f5c5ea1d072aad208c7b8
jrincondev <jrincondev@gmail.com>
2024-09-15 08:21:11 -0500

    Update

--- metodosArray.js
+++ metodosArray.js
@@ -1,5 +1,5 @@
 // Transformadores
-// pop( ) => elimina el último elemento de un array y lo devuelve.
+/* pop( ) => elimina el último elemento de un array y lo devuelve.
 
 let nombres = ["pedro", "brayan", "jorge"];
 
@@ -9,9 +9,129 @@ resultado = nombres.pop();
 
 console.log(resultado);
 console.log(nombres);
-// shift( ) => elimina el primer elemento de un array y lo devuelve.
-// push( ) => agrega un elemento a1 array a1 final de la lista.
-// reverse( ) => invierte el orden de los elementos de un array.
-// unshift( ) => agrega uno o más elementos a1 inicio del array, y devuelve la nueva longitud del arraya
-// sort() => ordena los elementos de un arreglo (array) localmente y devuelve el arreglo ordenado.
-// splice( ) => cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
+-----------------------------------*/
+
+/* shift( ) => elimina el primer elemento de un array y lo devuelve.
+
+let nombres = ["adrian", "josue", "francia"];
+
+console.log(nombres);
+
+resultado = nombres.shift();
+
+console.log(resultado);
+console.log(nombres);
+------------------------------------*/
+
+/* push( ) => agrega un elemento a1 array a1 final de la lista.
+
+let nombres = ["brayan", "andrea", "david"];
+
+console.log(nombres);
+
+resultado = nombres.push("jose", "elena");
+
+console.log(resultado);
+console.log(nombres);
+---------------------------------------*/
+
+/* reverse( ) => invierte el orden de los elementos de un array.
+
+let numeros = [1,2,3,4,5];
+
+console.log(numeros);
+
+resultado = numeros.reverse();
+
+console.log(resultado);
+----------------------------------*/
+
+/* unshift( ) => agrega uno o más elementos a1 inicio del array, y devuelve la nueva longitud del array
+
+let nombres = ["brayan", "andrea", "david"];
+
+console.log(nombres);
+
+resultado = nombres.unshift("jose", "elena");
+
+console.log(resultado);
+console.log(nombres);
+-------------------------------*/
+
+/* sort() => ordena los elementos de un arreglo (array) localmente y devuelve el arreglo ordenado.
+
+let nombres = ["brayan", "andrea", "david"];
+
+console.log(nombres);
+
+resultado = nombres.sort();
+
+console.log(resultado);
+--------------------------------*/
+
+/* splice( ) => cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
+
+let nombres = ["brayan", "andrea", "david","jorge"];
+
+console.log(nombres);
+
+resultado = nombres.splice(3,1,"jose", "elena");
+
+console.log(resultado);
+console.log(nombres);
+----------------------------------*/
+
+// Accesores
+/* join( ) => une todos los elementos de una matriz (u objeto similar) en una cadena y la devuelve.
+
+let nombres = ["brayan", "andrea", "david","jorge"];
+
+console.log(nombres);
+
+resultado = nombres.join(" - $");
+
+console.log(resultado);
+--------------------------------*/
+
+/* slice() => devuelve una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido).
+
+let nombres = ["brayan", "andrea", "david","jorge"];
+
+
+resultado = nombres.slice(1);
+
+console.log(resultado);
+---------------------------------*/
+
+/* - Metodos ya vistos en cadenas: tostring(), indexof(), lastlndexof(), includes()
+
+let nombres = ["brayan", "andrea", "david","jorge","david"];
+
+resultado1 = nombres.includes("david");
+resultado2 = nombres.indexOf("david");
+resultado3 = nombres.lastIndexOf("david");
+resultado4 = nombres.toString();
+
+console.log(resultado4);
+--------------------------------*/
+
+// De Repetición
+/* filter( ) => crea un nuevo array con todos los elementos que cumplan la condición.
+
+let nombres = ["brayan", "andrea", "david","jorge","federico"];
+
+resultado = nombres.filter(numero => numero.length > 5);
+resultado1 = nombres.filter(
+    (numero) => {
+    numero.length > 6
+});
+
+console.log(resultado);
+------------------------------*/
+
+/* forEach( ) => ejecuta la función indicada una vez por cada elemento del array.
+
+let nombres = ["brayan", "andrea", "david","jorge","federico"];
+
+nombres.forEach(numero => console.log(numero));
+------------------------------*/
---
+++ objetoMath.js
@@ -0,0 +1,31 @@
+// METODOS
+/* sqrt() => Devuelve la raiz cuadrada positiva de un número.
+
+numero = Math.sqrt(40);
+
+console.log(numero);
+----------------------*/
+
+// cbrt() => Devuelve la raíz cúbica de un número.
+
+numero = Math.cbrt(27);
+
+console.log(numero);
+
+// max() => Devuelve el mayor de cero o más números.
+// min() => Devuelve el más pequeño de cero o más números.
+// random( ) => Devue1ve un número pseudo-aleatorio entre O y 1.
+// round( ) => Devuelve el valor de un número redondeado a1 número entero más cercano.
+// fround( ) => Devuelve la representación flotante de precisión simple más cercana de un número.
+// floor( ) => Devuelve el mayor entero menor que o igual a un número.
+// trunc ( ) => Devuelve la parte entera del número x, la eliminación de los dígitos fraccionarios.
+
+
+// PROPIEDADES
+// PI
+// SQRT1_2
+// SQRT2
+// LN2
+// LNIO
+// LOG2E
+// LOGIOE
